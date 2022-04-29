c02dc27df42afceec75213f3aa7322ecc0645852
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-04-29 14:15:37 +0200

    get email from api

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const app = new Vue(
     {
         el: '#app',
-        data: {},
+        data: {
+            email: null
+        },
         methods: {},
         mounted() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => {
                 console.log(this);
                 console.log(response);
+                this.email = response.data.response
             })
         }
     }
